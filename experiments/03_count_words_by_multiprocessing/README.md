3e1211af79b2ca4f1489902b5077813879b0fe5e
kiselevskaya <kiselevskayaviktoriya@gmail.com>
2017-12-24 01:06:13 +0300

    Count words in list

---
+++ experiments/03_count_words_by_multiprocessing/README.md
@@ -0,0 +1,3 @@
+Count words from list
+
+Put words from the list in a queue as tasks. Initialize <n> processes. Every process gets the word from the queue and compares it with every word in the text. If there is a coincidence, the counter of this word increase. Put results in another queue and read one by one.
---
+++ experiments/03_count_words_by_multiprocessing/solution.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import threading
+import string
+import re
+import time
+from multiprocessing import Process, JoinableQueue
+
+file = open('../00_text_files/war-and-peace-I.txt', 'rt')
+content = file.read()
+file.close()
+text = content.split()
+new_text = []
+for word in text:
+    word = re.sub('['+string.punctuation+']', '', word).replace('–', '').replace('«', '').replace('»', '').replace('…', '').lower()
+    if word != "":
+        new_text.append(word)
+text = new_text
+
+words = ['я', 'ты', 'дети', 'а', 'от', 'к', 'до', 'по', 'с', 'ты', 'дети', 'а', 'от', 'к', 'до', 'по', 'с']
+
+def calculon(text, word):
+    result = 0
+    for item in text:
+        if item == word:
+            result += 1
+    return result
+
+q = JoinableQueue()
+ans = JoinableQueue()
+
+for word in words:
+    q.put(word)
+
+def full_calc(text, q, ans):
+    while True:
+        word = q.get()
+        if word is None:
+            break
+        count = calculon(text, word)
+        ans.put([word, count])
+        q.task_done()
+
+thread_count = 8
+threads = []
+for i in range(thread_count):
+    thread = Process(target=full_calc, args=(text, q, ans))
+    threads.append(thread)
+    thread.start()
+
+q.join
+
+for i in range(thread_count):
+    q.put(None)
+
+for thread in threads:
+    thread.join()
+
+while not ans.empty():
+    print(ans.get())
