935d4bc17b5366bcb74acd4b5ad3968b90e69c0d
kiselevskaya <kiselevskayaviktoriya@gmail.com>
2017-12-24 15:15:15 +0300

    New file with digits from another file

---
+++ experiments/05_create_text_with_digits_from_file/README.md
@@ -0,0 +1,3 @@
+Find digits in file
+
+Open <file> and search for digits. Results put in <new file> one by one. If digit equal 0, start with a new line. The last line in the <new file> is empty.
---
+++ experiments/05_create_text_with_digits_from_file/solution.py
@@ -0,0 +1,16 @@
+import string
+import re
+
+file = open('../00_text_files/war-and-peace-I.txt', 'rt')
+content = file.read()
+file.close()
+
+new_file = open('../00_text_files/out.txt', 'w+')
+
+digits = re.findall(r'\d', content)
+for i in digits:
+    new_file.write(i)
+    if i is '0':
+        new_file.write('\n')
+new_file.write('\n')
+new_file.close
