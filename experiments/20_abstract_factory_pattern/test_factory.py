3b3a34c0547002e3ade86ad57ac5301afa1b8ec1
kiselevskaya <kiselevskayaviktoriya@gmail.com>
2018-06-07 20:47:45 +0300

    unit tests for animals age

---
+++ experiments/20_abstract_factory_pattern/test_factory.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# test_factory.py
+
+
+import unittest
+from factory import *
+
+
+class AgeLimitTestCase(unittest.TestCase):
+    pet_factory = PetFactory()
+
+    def test_check_min_age(self):
+        self.assertEqual(self.pet_factory.min_age, month)
+
+    def test_check_wolf_age(self):
+        wolf = self.pet_factory.create_wolf()
+        self.assertTrue(wolf.get_age() <= self.pet_factory.wolf_max_age)
+
+    def test_check_lizard_age(self):
+        lizard = self.pet_factory.create_lizard()
+        self.assertTrue(lizard.get_age() <= self.pet_factory.lizard_max_age)
+
+    def test_check_parrot_age(self):
+        parrot = self.pet_factory.create_parrot()
+        self.assertTrue(parrot.get_age() <= self.pet_factory.parrot_max_age)
+
+    def test_check_raven_age(self):
+        raven = self.pet_factory.create_raven()
+        self.assertTrue(raven.get_age() <= self.pet_factory.raven_max_age)
