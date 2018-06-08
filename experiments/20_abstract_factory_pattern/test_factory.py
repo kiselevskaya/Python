00105bde6c5c39d7c01fe2488300f98089a8d0fc
kiselevskaya <kiselevskayaviktoriya@gmail.com>
2018-06-09 00:42:10 +0300

    added unittests

--- experiments/20_abstract_factory_pattern/test_factory.py
+++ experiments/20_abstract_factory_pattern/test_factory.py
@@ -4,10 +4,47 @@
 
 import unittest
 from factory import *
+from decorators import repeat
 
 
 class AgeLimitTestCase(unittest.TestCase):
     pet_factory = PetFactory()
+
+    @repeat(5)
+    def test_wolf_gender_not_none(self):
+        wolf = self.pet_factory.create_wolf()
+        self.assertIsNotNone(wolf.get_gender)
+
+    @repeat(5)
+    def test_lizard_gender_not_none(self):
+        lizard = self.pet_factory.create_lizard()
+        self.assertIsNotNone(lizard.get_gender)
+
+    @repeat(5)
+    def test_raven_gender_not_none(self):
+        raven = self.pet_factory.create_raven()
+        self.assertIsNotNone(raven.get_gender)
+
+    @repeat(5)
+    def test_parrot_gender_not_none(self):
+        parrot = self.pet_factory.create_parrot()
+        self.assertIsNotNone(parrot.get_gender)
+
+    def test_wolf_name_not_none(self):
+        wolf = self.pet_factory.create_wolf()
+        self.assertIsNotNone(wolf.get_name)
+
+    def test_lizard_name_not_none(self):
+        lizard = self.pet_factory.create_lizard()
+        self.assertIsNotNone(lizard.get_name)
+
+    def test_raven_name_not_none(self):
+        raven = self.pet_factory.create_raven()
+        self.assertIsNotNone(raven.get_name)
+
+    def test_parrot_name_not_none(self):
+        parrot = self.pet_factory.create_parrot()
+        self.assertIsNotNone(parrot.get_name)
 
     def test_check_min_age(self):
         self.assertEqual(self.pet_factory.min_age, month)
@@ -15,6 +52,7 @@ class AgeLimitTestCase(unittest.TestCase):
     def test_check_wolf_age(self):
         wolf = self.pet_factory.create_wolf()
         self.assertTrue(wolf.get_age() <= self.pet_factory.wolf_max_age)
+        self.assertTrue(wolf.get_age() >= self.pet_factory.min_age)
 
     def test_check_lizard_age(self):
         lizard = self.pet_factory.create_lizard()
