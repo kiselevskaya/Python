b0b60ca7bbb1237d8d97a348eed25a9ffe19bc75
kiselevskaya <kiselevskayaviktoriya@gmail.com>
2018-06-29 19:22:20 +0300

    added logs

--- experiments/20_abstract_factory_pattern/pet_shop.py
+++ experiments/20_abstract_factory_pattern/pet_shop.py
@@ -3,6 +3,7 @@
 
 
 import random
+import time
 
 
 class AbstractRandomFactory(object):
@@ -17,6 +18,7 @@ class RealRandomFactory(AbstractRandomFactory):
 
 class Shop:
     def __init__(self, pet_factory, animals_array, random_factory=RealRandomFactory()):
+        self.logs = []
         self.pet_factory = pet_factory
         self.animals_array = animals_array
         self.random_factory = random_factory
@@ -30,22 +32,32 @@ class Shop:
     def get_animals(self):
         return self.animals_array
 
+    def get_logs(self):
+        return self.logs
+
     def incident(self):
         if len(self.animals_array) >= 2:
             hungry = self.random_factory.choice(self.animals_array)
             food = self.random_factory.choice(self.animals_array)
             if hungry != food and hungry.weight >= (food.weight*2):
                 hungry.weight += food.weight
-                print('\n ', ('{} {} ♻ swallowed up ♻ {} {}'.format(hungry.species, hungry.name,
-                                                                    food.species, food.name)).center(83))
+                localtime = time.asctime(time.localtime(time.time()))
+                log = '{} {} ♻ swallowed up ♻ {} {}'.format(hungry.species, hungry.name, food.species, food.name)
+                time_log = '{}: {}'.format(localtime, log)
+                print('\n ', log.center(83))
                 self.animals_array.pop(self.animals_array.index(food))
+                self.logs.append(time_log)
 
     def reproduction(self):
         x = self.random_factory.choice(self.animals_array)
         y = self.random_factory.choice(self.animals_array)
         if x != y and x.gender != y.gender and abs(x.weight-y.weight) <= min(x.weight, y.weight):
             strength = random.randint(1, 4)
-            print('\n', ('{} ※ newborns ※'.format(strength)).center(83))
+            localtime = time.asctime(time.localtime(time.time()))
+            log = '{} {} & {} {} have {} ※ newborns ※'.format(x.species, x.name, y.species, y.name, strength)
+            time_log = '{}: {}'.format(localtime, log)
+            self.logs.append(time_log)
+            print('\n', log.center(83))
             num = 0
             while num != strength:
                 num += 1
@@ -67,8 +79,16 @@ class Shop:
         for animal in animals:
             animal.aging()
             animal.growth()
-            if animal.old_age() or animal.too_fat():
+            if animal.old_age():
+                localtime = time.asctime(time.localtime(time.time()))
+                log = '{}: ☠RIP☠ {} {} was {} years old, it was too old for {}.'.format(localtime, animal.species, animal.name, animal.age, animal.species)
+                self.logs.append(log)
                 animals.pop(animals.index(animal))  # to check
+            elif animal.too_fat():
+                localtime = time.asctime(time.localtime(time.time()))
+                log = '{}: ☠RIP☠ {} {} weighted {} kg, it was too fat for {}.'.format(localtime, animal.species, animal.name, animal.weight, animal.species)
+                self.logs.append(log)
+                animals.pop(animals.index(animal))
         self.incident()
         self.reproduction()
 
