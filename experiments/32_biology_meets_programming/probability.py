f67bd69ca0e4870cd1ea7fa53aa0a63869c8eeab
kiselevskaya <kiselevskaya.viktoriya@gmail.com>
2020-01-21 16:23:44 +0000

    rewrited function to find the most probable k-mer in text using profile-matrix

--- experiments/32_biology_meets_programming/probability.py
+++ experiments/32_biology_meets_programming/probability.py
@@ -19,17 +19,29 @@ profile = {'A': [0.2, 0.2, 0.0, 0.0, 0.0, 0.0, 0.9, 0.1, 0.1, 0.1, 0.3, 0.0],
 # print(probability(motif, profile))
 
 
-def profile_most_probable_kmer(text, k, profile):
-    motifs = frequent_words(text, k)
-    most_probable = ''
-    frequency = 0
-    for i in range(len(motifs)):
-        motif = motifs[i]
-        if probability(motif, profile) > frequency:
-            frequency = probability(motif, profile)
-            most_probable = motif
-    if most_probable == '':
-        most_probable = motifs[0]
+# def profile_most_probable_kmer(text, k, profile):
+#     motifs = frequent_words(text, k)
+#     most_probable = ''
+#     frequency = 0
+#     for i in range(len(motifs)):
+#         motif = motifs[i]
+#         if probability(motif, profile) > frequency:
+#             frequency = probability(motif, profile)
+#             most_probable = motif
+#     if most_probable == '':
+#         most_probable = motifs[0]
+#     return most_probable
+
+
+def profile_most_probable_pattern(text, k, profile):
+    p = -1
+    most_probable = text[0:k]
+    for i in range(len(text)-k+1):
+        motif_to_compare = text[i:i+k]
+        pr = probability(motif_to_compare, profile)
+        if pr > p:
+            p = pr
+            most_probable = motif_to_compare
     return most_probable
 
 
