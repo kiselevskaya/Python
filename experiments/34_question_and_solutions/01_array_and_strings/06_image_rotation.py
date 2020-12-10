ed06982cdd28616d542a19dbf3a11c787b7eaf1d
kiselevskaya <viktoruia.kisialeuskaya@gmail.com>
2020-12-10 15:26:24 +0000

    Added solution to image matrix NxM rotation by 90 degree

---
+++ experiments/34_question_and_solutions/01_array_and_strings/06_image_rotation.py
@@ -0,0 +1,117 @@
+
+'''Given an image represented by an NxM matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degree.
+    Can you do it in place?
+
+    Let N be image width, M - image height
+'''
+
+
+def rotate_image_clockwise(matrix, clockwise=True):
+    n, m = len(matrix), len(matrix[0])
+    # Rotate image
+    if clockwise:
+        # Column of pixels by column
+        # For rotate image by 90 degree clockwise left image side (column 0) is going to be on top (row 0)
+        # so the first pixel of last row will be the first pixel in the first row etc.
+        matrix = [[matrix[j-1][i] for j in range(n, 0, -1)] for i in range(m)]
+    else:
+        # Row of pixels by row
+        matrix = [[matrix[j][i] for j in range(n)] for i in range(m)][::-1]
+    return matrix
+
+
+def create_matrix(n=4, m=4):
+    import string
+    # Alphabet string
+    alpha = string.ascii_uppercase
+    # Create template matrix for image (represented as nested list)
+    matrix = [[alpha[j] for j in range(i*n, n*(i+1))] for i in range(m)]
+    return matrix
+
+
+def represent_image(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            print(matrix[i][j], end=' ')
+        print()
+    print('-'*2*len(matrix[0]))
+
+
+# Suggested solution (anti-clockwise)
+# https://www.geeksforgeeks.org/inplace-rotate-square-matrix-by-90-degrees/
+def rotateMatrix(mat, N=4):
+    # Consider all squares one by one
+    for x in range(0, int(N / 2)):
+        # Consider elements in group
+        # of 4 in current square
+        for y in range(x, N-x-1):
+            # store current cell in temp variable
+            temp = mat[x][y]
+            # move values from right to top
+            mat[x][y] = mat[y][N-1-x]
+            # move values from bottom to right
+            mat[y][N-1-x] = mat[N-1-x][N-1-y]
+            # move values from left to bottom
+            mat[N-1-x][N-1-y] = mat[N-1-y][x]
+            # assign temp to left
+            mat[N-1-y][x] = temp
+
+
+if __name__ == '__main__':
+    # Test case 0
+    matrix = create_matrix(4, 4)
+    represent_image(matrix)
+    rotated = rotate_image_clockwise(matrix)
+    represent_image(rotated)
+
+    # Test case 1
+    matrix1 = create_matrix(2, 4)
+    represent_image(matrix1)
+    rotated = rotate_image_clockwise(matrix1)
+    represent_image(rotated)
+
+    # Test case 2
+    matrix2 = create_matrix(5, 2)
+    represent_image(matrix2)
+    rotated = rotate_image_clockwise(matrix2)
+    represent_image(rotated)
+
+    # Test case 3
+    matrix3 = [[1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]]
+    represent_image(matrix3)
+    rotated = rotate_image_clockwise(matrix3, False)
+    represent_image(rotated)
+
+    # Test case 4
+    matrix4 = [[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]]
+    represent_image(matrix4)
+    rotated = rotate_image_clockwise(matrix4, False)
+    represent_image(rotated)
+
+    # Test case 5
+    matrix5 = [[1, 2],
+            [4, 5]]
+    represent_image(matrix5)
+    rotated = rotate_image_clockwise(matrix5, False)
+    represent_image(rotated)
+
+    # Test case 6
+    matrix6 = [[1, 2, 3],
+            [5, 6, 7],
+            [9, 10, 11],
+            [13, 14, 15]]
+    represent_image(matrix6)
+    rotated = rotate_image_clockwise(matrix6, False)
+    represent_image(rotated)
+
+    # Test case 7
+    matrix7 = [[1, 2, 3, 4],
+            [5, 6, 7, 8]]
+    represent_image(matrix7)
+    rotated = rotate_image_clockwise(matrix7, False)
+    represent_image(rotated)
--- experiments/34_question_and_solutions/01_array_and_strings/README.md
+++ experiments/34_question_and_solutions/01_array_and_strings/README.md
@@ -1,18 +1,20 @@
 
 
-01_unique_characters.py
+**01_unique_characters.py**
+
     Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
 
-#02_
-    Implement a function void reverse(char*str) in C or C++ which reverses a null-terminated string.
+#02 (Implement a function void reverse(char*str) in C or C++ which reverses a null-terminated string.)
 
 
-03_string_permutation.py
+**03_string_permutation.py**
+
     Given two strings, write a method to decide if one is a permutation of the other.
 
 
-04_replace_spaces.py
+**04_replace_spaces.py**
+
     Write a method to replace all spaces in a string with '%20'.
     You may assume that the string has sufficient space at the end of the string to hold the additional characters,
     and that you are given the 'true' length of the string.
@@ -21,10 +23,14 @@
     Output:'Mr%20John%20Smith'
 
 
-05_string_compression.py
+**05_string_compression.py**
+
     Implement a method to perform basic string compression using the counts of repeated characters.
     For example, the string 'aabcccccaaa' would become 'a2b1c5a3'.
     If the "compressed" string would not become smaller than the original string, your method should return the original string.
 
 
-06_
+**06_image_rotation.py**
+
+    Given an image represented by an NxM matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degree.
+    Can you do it in place?
